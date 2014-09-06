bd1e07c0cf457bbd89adf90d9d6dfbd40dad701b
Tom Regan <code.tom.regan@gmail.com>
2014-09-06 11:48:17 +0100

    make output directory configurable; remove logger; validate command line arguments

--- root.py
+++ root.py
@@ -18,64 +18,135 @@ import sys
 import zipfile
 import logging
 import xml.etree.ElementTree as ET
-import json
+import yaml
 
 from os import path
 from os import walk
+from os import makedirs
 from os.path import join
+from shutil import copy2 as copy
 
 
-def load_metadata(epub_filename, search=lambda x, y: x[y]):
+def load_metadata(epub_filename, search):
     """Reads an epub file and returns its OPS / OEBPS blob"""
+    if not zipfile.is_zipfile(epub_filename):
+        print "%s is not an epub file" % epub_filename
+        return
     with zipfile.ZipFile(epub_filename, 'r') as epub_file:
         meta_data = None
         try:
             meta_data = epub_file.read("META-INF/container.xml")
         except Exception, e:
-            logging.error(
-                "Could not locate a container file in the epub bundle")
-            return None
+            print "Could not locate a container file in the epub bundle"
+            return
         meta_xml = ET.fromstring(meta_data)
-        full_path = search(meta_xml, "rootfile").attrib["full-path"]
+        full_path = search(meta_xml, "rootfile")
         if full_path is None:
-            logging.error(
-                "Could not locate a metadata file in the epub bundle")
-            return None
-        return ET.fromstring(epub_file.read(full_path))
+            print "Could not locate a metadata file in the epub bundle"
+            return
+        return ET.fromstring(epub_file.read(full_path.attrib["full-path"]))
 
-def isbn(number, old_length=0):
-    """Return an ISBN given a (possibly malformed) string"""
+def _isbn(number, old_length=0):
+    """Return an ISBN given a (possibly malformed) string
+    """
     length = len(number)
     if length == old_length:
-        return None
-    if length == 13 or length == 10:
         return int(number)
-    return isbn(''.join([x for x in number if x.isdigit()]), length)
+    if number.isdigit() and (length == 13 or length == 10):
+        return int(number)
+    return _isbn(''.join([x for x in number if x.isdigit()]), length)
 
-def load_ops_data(xml_data, search=lambda x, y: x[y]):
-    """Constructs a dictionary from OPS XML data"""
+def _author(string):
+    """Return the normalised author name
+    """
+    name = string.split(',')
+    return len(name) == 1  and string or ' '.join([name[1], name[0]]).strip()
+
+def load_ops_data(xml_data, search):
+    """Constructs a dictionary from OPS XML data
+    """
+    title = search(xml_data, "title")
+    author = search(xml_data, "creator")
+    isbn = search(xml_data, "identifier")
     return {
-        "title": search(xml_data, "title").text,
-        "author": search(xml_data, "creator").text,
-        "date": search(xml_data, "date").text,
-        "ISBN": isbn(search(xml_data, "identifier").text),
-        "language": search(xml_data, "language").text
+        "title": title is None and None or title.text,
+        "author": author is None and None or _author(author.text),
+        "ISBN": isbn is not None and _isbn(isbn.text) or None
     }
+
+def move_to_library(srcpath, book):
+    """Move files to the library
+    """
+    config = None
+    try:
+        with open("_config.yaml") as config_file:
+            config = yaml.safe_load(config_file)
+    except Exception, e:
+        print "Failed to load configuration"
+        return
+    destination_dir = path.join(config['directory'], book['author'])
+    destination_file = "%s.%s" % (book['title'], "epub")
+    destpath = path.join(destination_dir, destination_file)
+    try:
+        if not path.exists(destination_dir):
+            makedirs(destination_dir)
+    except OSError, e:
+        print "Error creating path %s" % destination_dir
+        return
+    print "%s ->\n%s" % (path.basename(srcpath), destpath)
+    try:
+        copy(srcpath, destpath)
+    except IOError, e:
+        print "Error copying %s (%s)" % (srcpath, e.errno)
+
+def usage(message=None):
+    """Prints a usage message
+    """
+    if message is not None:
+        print message
+    print """Usage
+Do stuff
+"""
 
 def main(args):
     """Does the main things."""
-    logging.basicConfig(level=logging.DEBUG)
 
-    locate_element = lambda x, y: [e for e in x.iter() if e.tag.endswith(y)][0]
+    if len(args) < 2:
+        usage()
+        return
 
-    for (dirpath, dirnames, filenames) in walk(args[1]):
+    command = args[1]
+
+    if command not in ["import"]:
+        usage("%s is not a command" % command)
+        return
+
+    if len(args) < 3:
+        usage("%d is not the right number of arguments for %s"
+              % (len(args) - 2, command))
+        return
+
+    dirpath = args[2]
+
+    def locate_element(x, y):
+        if x is None or y is None:
+            return
+        elements = [e for e in x.iter() if e.tag.endswith(y)]
+        if elements is None or len(elements) < 1:
+            return
+        return elements[0]
+
+
+    for (dirpath, dirnames, filenames) in walk(dirpath):
         for filename in filenames:
             if filename.endswith(".epub"):
-                content_xml = load_metadata(
-                    join(dirpath, filename), search=locate_element)
+                filepath = join(dirpath, filename)
+                content_xml = load_metadata(filepath, search=locate_element)
+                if content_xml is None:
+                    print "Failed to process %s" % filename
+                    continue
                 book = load_ops_data(content_xml, search=locate_element)
-                print path.basename(filename)
-                print json.dumps(book, sort_keys=True, indent=2)
+                move_to_library(filepath, book)
 
 if __name__ == '__main__':
     main(sys.argv)
