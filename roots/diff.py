609ebec38f14b3cf3980b7927b2a22dd1136b923
Tom Regan <code.tom.regan@gmail.com>
2015-07-29 22:14:33 +0100

    starting to diff books

---
+++ roots/diff.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2015 Tom Regan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import namedtuple
+from difflib import SequenceMatcher
+
+Add = namedtuple('Add', 'text')
+Same = namedtuple('Same', 'text')
+Delete = namedtuple('Delete', 'text')
+Replace = namedtuple('Replace', 'text')
+
+def diff(original, replacement):
+    result = {}
+    for key in replacement.keys():
+        if not key in original and key in replacement:
+            result[key] = [Add(replacement[key])]
+        if key in original and key in replacement:
+            matcher = SequenceMatcher(
+                a=original[key],
+                b=replacement[key])
+            opcodes = matcher.get_opcodes()
+            chunks = []
+            for opcode in opcodes:
+                if opcode[0] == 'insert':
+                    start, end = opcode[3], opcode[4]
+                    chunks.append(Add(replacement[key][start:end]))
+                if opcode[0] == 'replace':
+                    start, end = opcode[3], opcode[4]
+                    chunks.append(Replace(replacement[key][start:end]))
+                if opcode[0] == 'delete':
+                    start, end = opcode[1], opcode[2]
+                    chunks.append(Delete(original[key][start:end]))
+                if opcode[0] == 'equal':
+                    start, end = opcode[1], opcode[2]
+                    chunks.append(Same(original[key][start:end]))
+                result[key] = chunks
+    return result
---
+++ roots/test_diff.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2015 Tom Regan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from diff import diff, Add, Same, Delete, Replace
+
+class TestDiff(unittest.TestCase):
+    def test_addition_is_tagged(self):
+        expected = {
+            'title': [Add('Gone Girl')],
+            'author': [Add('Gillian Flynn')]
+        }
+        actual = diff({}, {
+            'title': 'Gone Girl',
+            'author': 'Gillian Flynn'
+        })
+        self.assertEqual(expected, actual)
+        self.equalTypes(expected, actual)
+
+    def test_removal_is_tagged(self):
+        expected = {'title': [Same('Gone'), Delete(' Girl')]}
+        actual = diff({'title': 'Gone Girl'}, {'title': 'Gone'})
+        self.assertEqual(expected, actual)
+        self.equalTypes(expected, actual)
+
+    def test_insertion_is_tagged(self):
+        expected = {'title': [Add('Gone '), Same('Girl')]}
+        actual = diff({'title': 'Girl'}, {'title': 'Gone Girl'})
+        self.assertEqual(expected, actual)
+        self.equalTypes(expected, actual)
+
+    def test_replacement_is_tagged(self):
+        expected = {'title': [Same('G'), Replace('irl')]}
+        actual = diff({'title': 'Gone'}, {'title': 'Girl'})
+        self.assertEqual(expected, actual)
+        self.equalTypes(expected, actual)
+
+    def equalTypes(self, a, b):
+        for k in a.keys():
+            [self.assertTrue(x) for x in
+             [type(x) == type(y) for (x, y) in zip(a[k], b[k])]
+            ]
